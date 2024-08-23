31aa839de801373c9f4c47a5f8b0901073d32e63
liuyujie0136 <liuyujie3201@outlook.com>
2024-08-23 16:32:59 +0800

    clean files

---
+++ JCR_data/makeDBJSfromJCR.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat JCR2024_parsed.txt | tail -n +2 | grep -vw NA | awk -v q="'" 'BEGIN{FS="\t"; OFS=""; ORS=""; print "var dataSource = {"}; {print q,$1,q": [",$2,", ",$3,", ",q,$4,q,"], "}; END{print "};"}' > ../db.js
